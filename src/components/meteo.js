4a8edeeca40003a7b8a19bdf47f89daecbe831db
Jules DAYAUX <julesdayaux@MacBook-Pro-de-Jules.local>
2021-02-02 15:00:05 +0100

    [add] weather app without integration

---
+++ src/components/meteo.js
@@ -0,0 +1,65 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const api_url = process.env.REACT_APP_API_URL
+const api_key = process.env.REACT_APP_API_KEY
+export default function Meteo() {
+    const [fetched, setFetched] = useState(false)
+    const [meteo, setMeteo] = useState(1)
+    useEffect(() => {
+        if (!fetched) {
+            getLocation()
+        }
+    })
+
+    async function getLocation() {
+        if (!fetched) {
+            if (navigator.geolocation) {
+                await navigator.geolocation.getCurrentPosition((position) => {
+                    fetchURl(position.coords.latitude, position.coords.longitude)
+                }, error);
+            }
+            setFetched(true)
+        }
+    }
+
+    function fetchURl(lat, lng) {
+        const url = `${api_url}lat=${lat}&lon=${lng}&units=metric&appid=${api_key}`
+        axios.get(url)
+            .then(res => {
+                let meteoResponse = res.data.weather[0].main
+                let temp = res.data.main.temp
+                let icon = res.data.weather[0].icon
+                let town = res.data.name
+                setMeteo({
+                    town: town,
+                    weather: meteoResponse,
+                    temperature: temp,
+                    icon: icon
+                })
+            })
+    }
+
+    function error() {
+        alert("Geolocalisation impossible");
+    }
+
+    return (
+        <div>
+            <h1>
+                {
+                    typeof (meteo) === "object"
+                        ?
+                        <div>
+                            <h1>{"A " + meteo.town + ", il fait " + Math.ceil(meteo.temperature) + "°C"}</h1>
+                            <span><strong>{meteo.weather}</strong></span>
+                            <img src={"http://openweathermap.org/img/wn/" + meteo.icon + ".png"}
+                                 alt={meteo.town + " icon"}/>
+                        </div>
+                        :
+                        "pas tableau"
+                }
+            </h1>
+        </div>
+    )
+}
