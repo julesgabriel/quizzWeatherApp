80bd8a144226c0e9327a210bcc207088d6699cc2
Cyril-lamirand <contact.cyril1@gmail.com>
2021-02-07 23:40:10 +0100

    [Update] Quizz.js
    - Remove unused Function()

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -2,9 +2,6 @@ import {Questions} from "./Questions";
 import React from "react";
 
 export const Quizz = (props) => {
-    function handleSubmit() {
-
-    }
     const {data} = props;
     return (
         <div>
